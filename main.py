e88213e4973b0597d752b58aa0fe9454ae545e64
Tuna Makaracı <tunainnebula@gmail.com>
2023-12-18 04:41:35 +0300

    update

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 # SIMPLE BLACKJACK GAME
-
 from tkinter import *
-import tkinter
 import random
 
 window = Tk()
@@ -14,38 +12,75 @@ label.config(bg="black")
 label.config(fg="white")
 label.pack()
 
+user_card = 0
+cpu_card = 0
+
+user_card_list = []
+
+cpu_card_list = []
+
 
 def button_clicked():
     print("button clicked")
 
 
-button = Button(text="ENTER", command=button_clicked)
-
-button.pack()
-
-user_score = 0
-cpu_score = 0
-
-
-# user_label = Label(text=f"{user_score}")
-# print("welcome to blackjack game")
-
-# print("lütfen başlamak için ENTER'a basın")
 
 def game_start():
-    user_score = random.randint(0, 21)
-    print(f"kartınız {user_score}")
-    user_label = Label(text=f"kartınız {user_score}")
-    user_label.config(bg="black")
-    user_label.config(fg="white")
-    user_label.pack()
-    cpu_score = random.randint(0, 21)
-    cpu_label = Label(text=f"rakibinizin kartı {cpu_score}")
-    cpu_label.config(bg="black")
-    cpu_label.config(fg="white")
-    cpu_label.pack()
-    print(f"bilgisayarın kartı {cpu_score}")
+    # USER CARD LABEL
+    your_card_label = Label(text="Kartlarınız") # your card label text
+    your_card_label.config(bg="black")
+    your_card_label.config(fg="white")
+    your_card_label.pack(side=LEFT)
+    # CPU CARD LABEL
+    cpu_card_label = Label(text="Rakibinizin Kartları") # your card label text
+    cpu_card_label.config(bg="black")
+    cpu_card_label.config(fg="white")
+    cpu_card_label.pack(side=RIGHT)
+
+
+
+def cont_button_clicked():
+    print("cont button clicked")  # button check
+    user_card = random.randint(1,10) # users card config
+    user_first_card = Label(text=f"{user_card}")
+    user_first_card.config(bg="black")
+    user_first_card.config(fg="white")
+    user_first_card.pack(side=LEFT, padx=5)
+    user_card_list.append(user_card)
+    print(user_card_list)
+    # CPU card
+    cpu_card = random.randint(1,10) # cpu card config
+    cpu_first_card = Label(text=f"{cpu_card}")
+    cpu_first_card.config(bg="black")
+    cpu_first_card.config(fg="white")
+    cpu_first_card.pack(side=RIGHT, padx=5)
+    cpu_card_list.append(cpu_card)
+    print(cpu_card_list)
+
+
+cont_button = Button(text="Kart Çek", command= cont_button_clicked)
+cont_button.pack()
+
+user_sum = sum(user_card_list)
+cpu_sum = sum(cpu_card_list)
+
+user_len = len(user_card_list)
+cpu_len = len(cpu_card_list)
+
+
+def game_end():
+    if user_sum > 21 and user_len > 2:
+        print("CPU kazandı")
+    elif cpu_sum > 21 and cpu_len > 2:
+        print("KAZANDINIZ")
+    elif user_sum == 21:
+        print("KAZANDINIZ")
+    elif cpu_sum == 21:
+        print("CPU kazndı")
+
+
 
 
 game_start()
+game_end()
 window.mainloop()
