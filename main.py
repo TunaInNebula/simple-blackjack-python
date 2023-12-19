dc907710dda0439f3aa5e48de79fe4580d02e18b
Tuna Makaracı <tunainnebula@gmail.com>
2023-12-19 22:07:10 +0300

    update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # SIMPLE BLACKJACK GAME
 from tkinter import *
 import random
+from tkinter import messagebox
 
 window = Tk()
 window.title("Python Blackjack")
@@ -12,6 +13,8 @@ label.config(bg="black")
 label.config(fg="white")
 label.pack()
 
+
+
 user_card = 0
 cpu_card = 0
 
@@ -20,8 +23,6 @@ user_card_list = []
 cpu_card_list = []
 
 
-def button_clicked():
-    print("button clicked")
 
 
 
@@ -40,10 +41,11 @@ def game_start():
 
 
 def cont_button_clicked():
+
     print("cont button clicked")  # button check
     user_card = random.randint(1,10) # users card config
     user_first_card = Label(text=f"{user_card}")
-    user_first_card.config(bg="black")
+    user_first_card.config(bg="green")
     user_first_card.config(fg="white")
     user_first_card.pack(side=LEFT, padx=5)
     user_card_list.append(user_card)
@@ -52,11 +54,12 @@ def cont_button_clicked():
     # Güncellenen user_sum
     global user_sum
     user_sum = sum(user_card_list)
+    user_score_label.config(text=f"Toplam: {user_sum}")
 
     # CPU card
     cpu_card = random.randint(1,10) # cpu card config
     cpu_first_card = Label(text=f"{cpu_card}")
-    cpu_first_card.config(bg="black")
+    cpu_first_card.config(bg="purple")
     cpu_first_card.config(fg="white")
     cpu_first_card.pack(side=RIGHT, padx=5)
     cpu_card_list.append(cpu_card)
@@ -66,19 +69,68 @@ def cont_button_clicked():
 
     global cpu_sum
     cpu_sum = sum(cpu_card_list)
+    cpu_score_label.config(text=f"Toplam: {cpu_sum}")
+
+
 
     #oyun sonu
 
     game_end()
 
 
+
+
 cont_button = Button(text="Kart Çek", command= cont_button_clicked)
 cont_button.pack()
+
+
+def stop_button_clicked():
+    print("stop button clicked")
+    global cpu_sum
+
+    if cpu_sum < 21 and cpu_sum < user_sum:
+        print("pass")
+        cpu_card = random.randint(1, 10)  # cpu card config
+        cpu_first_card = Label(text=f"{cpu_card}")
+        cpu_first_card.config(bg="black")
+        cpu_first_card.config(fg="white")
+        cpu_first_card.pack(side=RIGHT, padx=5)
+        cpu_card_list.append(cpu_card)
+        print(cpu_card_list)
+
+        cpu_sum = sum(cpu_card_list)
+        cpu_score_label.config(text=f"Toplam: {cpu_sum}")
+
+
+    if user_sum > cpu_sum:
+        print("KAZANDINIZ")
+        messagebox.showinfo(title="Decrypted Text", message="KAZANDINIZ")
+
+    elif cpu_sum > user_sum:
+        print("CPU kazandı")
+        messagebox.showinfo(title="Decrypted Text", message="CPU KAZANDI")
+
+    else:
+        print("BERABERE")
+        messagebox.showinfo(title="Decrypted Text", message="BERABERE")
+
+    game_end()
+
+
+
+
+
+stop_button = Button(text="Dur", command= stop_button_clicked)
+stop_button.pack()
 
 user_sum = sum(user_card_list)
 cpu_sum = sum(cpu_card_list)
 
+user_score_label = Label(text=f"toplam: {user_sum}")
+user_score_label.pack(side=LEFT, pady=15)
 
+cpu_score_label = Label(text=f"toplam: {cpu_sum}")
+cpu_score_label.pack(side=RIGHT, pady=15)
 
 
 def game_end():
@@ -86,19 +138,57 @@ def game_end():
     print(f"cpu sum: {cpu_sum} ")
     if user_sum > 21 >= cpu_sum:
         print("CPU kazandı")
+        messagebox.showinfo(title="Decrypted Text", message="CPU KAZANDI")
+
     elif cpu_sum > 21 >= user_sum:
-        print("KAZANDINIZ")
+        print("KAZANDINIZ")  # hatırlatma messagebox kullan
+        messagebox.showinfo(title="Decrypted Text", message="KAZANDINIZ")
+
     elif user_sum == 21 and cpu_sum != 21:
         print("KAZANDINIZ")
+        messagebox.showinfo(title="Decrypted Text", message="KAZANDINIZ")
+
     elif cpu_sum == 21 and user_sum != 21:
         print("CPU kazandı")
+        messagebox.showinfo(title="Decrypted Text", message="CPU KAZANDI")
+
     elif user_sum > 21 and cpu_sum > 21:
         print("BERABERE")
+        messagebox.showinfo(title="Decrypted Text", message="BERABERE")
 
+
+
+# ...
+
+def restart_game():
+
+    for widget in window.winfo_children():
+        if isinstance(widget, Label):
+            user_score_label.destroy()
+    # Temizleme işlemleri
+    user_card_list.clear()
+    cpu_card_list.clear()
+    user_sum = 0
+    cpu_sum = 0
+
+    # Metinleri güncelle
+    user_score_label.config(text=f"Toplam: {user_sum}")
+    cpu_score_label.config(text=f"Toplam: {cpu_sum}")
+
+    # ...
+    game_start()
+
+# ...
+
+restart_button = Button(text="Yeniden Başlat", command=restart_game)
+restart_button.pack()
+
+# ...
 
 
 
 
 game_start()
+
 
 window.mainloop()
